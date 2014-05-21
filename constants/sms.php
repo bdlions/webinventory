19d9f78a0c2074930250db395f272475b43dd311
nazmulhasan <nazmul@apurbatech.com>
2014-05-21 17:33:23 +0600

    sms configuration

--- constants/sms.php
+++ constants/sms.php
@@ -1,7 +1,10 @@
 <?php
-    define("SMS_MESSAGE_CATEGORY_CUSTOMER_REGISTRATION_TYPE_ID",       1);
-    define("SMS_MESSAGE_CATEGORY_SUPPLIER_REGISTRATION_TYPE_ID",       2);
+    define("SMS_MESSAGE_CATEGORY_CUSTOMER_REGISTRATION_TYPE_ID",           1);
+    define("SMS_MESSAGE_CATEGORY_SUPPLIER_REGISTRATION_TYPE_ID",           2);
     
-    define("SMS_CUSTOMER_REGISTRATION_MESSAGE",                        "Congratulation for successfully registration.");
-    define("SMS_SUPPLIER_REGISTRATION_MESSAGE",                        "We hope that we can establish a good business relationship with you.");
+    define("SMS_MESSAGE_CATEGORY_CUSTOMER_REGISTRATION_DESCRIPTION",       "Customer registration");
+    define("SMS_MESSAGE_CATEGORY_SUPPLIER_REGISTRATION_DESCRIPTION",       "Supplier registration");
+    
+    define("SMS_CUSTOMER_REGISTRATION_MESSAGE",                            "Congratulation for successfully registration.");
+    define("SMS_SUPPLIER_REGISTRATION_MESSAGE",                            "We hope that we can establish a good business relationship with you.");
 ?>
