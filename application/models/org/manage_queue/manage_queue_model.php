9e95b29c00699f4e633cf69e0db426b1501691b2
Redwanul Islam <raduan.bracu@gmail.com>
2014-05-24 12:36:08 +0600

    Mange_queue_model/library created

---
+++ application/models/org/manage_queue/manage_queue_model.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ *
+ * Requirements: PHP5 or above
+ *
+ */
+class Manage_queue_model extends Ion_auth_model {
+    public function __construct() {
+        parent::__construct();
+    }   
+    /**/
+    
+}
