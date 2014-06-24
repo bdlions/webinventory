b4da77ce031b8600534a89d7e36716ca45fb984e
Redwanul Islam <raduan.bracu@gmail.com>
2014-06-24 17:46:13 +0600

    Security check message updated

--- application/controllers/shop.php
+++ application/controllers/shop.php
@@ -37,10 +37,10 @@ class Shop extends CI_Controller {
         if(!empty($user_group))
         {
             $user_group = $user_group[0];
-            $this->session->set_flashdata('message',"You have no permission to view that page");
-            
+
             if($user_group['id'] == USER_GROUP_SALESMAN)
             {
+                $this->session->set_flashdata('message',"You have no permission to view that page");
                 redirect('user/salesman_login',"refresh");
             }
         }
