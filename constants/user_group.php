c4e4aaa5ca40df3cf9109df6c477c9b63d6d9e0d
nazmulhasan <nazmul@apurbatech.com>
2015-01-16 21:58:52 +0600

    extra files are removed

--- constants/user_group.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("USER_GROUP_ADMIN",                                   1);
-    define("USER_GROUP_MANAGER",                                 2);
-    define("USER_GROUP_SALESMAN",                                3);
-    define("USER_GROUP_SUPPLIER",                                4);
-    define("USER_GROUP_CUSTOMER",                                5);
-?>
