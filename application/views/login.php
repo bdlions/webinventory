048ec69dd47abb0a34986c4eebb6e15c28c01f0f
Redoy <nazmulislamredoy@gmail.com>
2015-03-11 18:16:37 +0600

    login page updates

--- application/views/login.php
+++ application/views/login.php
@@ -1,12 +1,12 @@
 <style>
     .clr{clear:both;}
-    *{margin:0 ; padding: 0 ; }
+    *{margin:0 ;}
     *img{ border:0; } 
 
-    .header{ background-color:#75B3E6;}
-    .ribbon_logo{ float:left; padding: 20px; }
+    .header{ background-color:#75B3E6; padding-top: 8px;}
+    .ribbon_logo{ float:left;}
     .ribbon_logo img{ float: left; }
-    .login{ float: left; padding: 20px; } 
+    .login{ float: left; } 
     .login div { float:left;}
     .empty1{ width: 150px; float:left;} 
 
@@ -16,7 +16,7 @@
     .login-color2{ color: white; font-size:14px; vertical-align: top;}
     .login-color2 a { color: white; font-size:14px; } 
 
-    .login-button{  background-color:#607cad; color:white;  border: 1px solid #5D6477; box-shadow:inset; text-align:center; width:48px; height: 20px; margin-top: 26px;}
+    .login-button{  background-color:#607cad; color:white;  border: 1px solid #5D6477; box-shadow:inset; text-align:center; width:48px; height: 20px; margin-top: 28px;}
     .login-button a { text-decoration:none ; color: white; }
     .content{ background-color: #edf0f5;}
     .empty2{ width: 500px; float:left;} 
@@ -37,96 +37,135 @@
     .footer{ height:140px; background-color:#F4F4F4; width:100%; }
 </style>
 <link rel="stylesheet" type="text/css" href="">
-<div class="col-md-12 container clr">
-    <div class="row">
-        <div class="header col-md-12">
-            <div class="ribbon_logo col-md-4 pull-left">
-                <img src="<?php echo base_url();?>resources/images/webinventory.png">
+
+<div class="container-fluid">
+    <div class="header_background">
+        <div class="row">
+            <div class="col-md-offset-1 col-md-5">
+                <div class="col-xs-12 col-sm-12">
+                    <img src="<?php echo base_url(); ?>resources/images/webinventory.png">
+                </div>
             </div>
-            <div class="login pull-right col-md-6">
+            <div class="col-md-offset-1 col-md-4">
                 <?php echo form_open(LOGIN_URI, array('id' => 'admin-login', 'role' => 'form', 'class' => 'form-horizontal')); ?>
-                <div class="row col-md-12">
-                    <div class="row col-md-4">
-                        <label> <span class="login-color1"><?php echo lang('login_identity_label', 'identity'); ?><br> </span> </label>
-                        <?php echo form_input($identity); ?><br>
-                        <input type="checkbox">
-                        <span class="login-color2"> keep me logged in</span>
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="col-xs-12 col-sm-12"><label> <span class="user_login_style"><?php echo lang('login_identity_label', 'identity'); ?></span> </label></div>
+                        <div class="col-xs-12 col-sm-12"><?php echo form_input($identity); ?></div>
+                        <div class="col-xs-12 col-sm-12">
+                            <input type="checkbox">
+                            <span class="user_password_style"> keep me logged in</span>
+                        </div>
                     </div>
-                    <div class="row col-md-4">
-                        <label> <span class="login-color1"> <?php echo lang('login_password_label', 'password'); ?><br> </span> </label>
-                        <?php echo form_input($password); ?><br>
-                        <span class="login-color2"><a href="#">Forgot your password? </a></span>
+                    <div class="col-md-5">
+                        <div class="col-xs-12 col-sm-12"><label> <span class="user_login_style"> <?php echo lang('login_password_label', 'password'); ?> </span> </label></div>
+                        <div class="col-xs-12 col-sm-12"><?php echo form_input($password); ?></div>
+                        <div class="col-xs-12 col-sm-12"><span class="user_password_style"><a href="#">Forgot your password? </a></span></div>
                     </div>
-                    <div class="row col-md-4">
-                        <input type="submit" value="Login" name="login_submit_btn" id="login_submit_btn" class="login-button">
+                    <div class="col-md-2">
+                        <div class="col-xs-12 col-sm-12" ><input type="submit" value="Login" name="login_submit_btn" id="login_submit_btn" class="login_button_style"></div>
                     </div>
                 </div>
-                <div class="row col-md-12 login-color1"><?php echo $message1; ?></div>
+                <div class="row form-group">
+                    <div class="col-md-12 col-xs-12 col-sm-12">
+                        <div class="user_login_style"><?php echo $message1; ?></div>
+                    </div>
+                </div>
                 <?php echo form_close(); ?>
             </div>
-            <br style="clear: both;">
-        </div>
-    </div>
-    <div class="row">
-        <div class="content clr col-md-12">
-            <div class="col-md-8" style="padding-top: 20px;">
-                <img class="img-responsive" src="<?php echo base_url();?>resources/images/login_left_image.jpg">
-            </div>
-            <div class="body_form col-md-4">
-                <?php echo form_open(LOGIN_URI, array('id' => 'form_create_admin', 'class' => 'form-horizontal')); ?>
-                    <h1> Sign Up </h1>
-                    <div class="row col-md-12 login-color"><?php echo $message2; ?></div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php echo form_input($first_name + array('class' => 'form-control', 'placeholder'=>'First Name')); ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php echo form_input($last_name + array('class' => 'form-control', 'placeholder'=>'Last Name')); ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php echo form_input($email + array('class' => 'form-control', 'placeholder'=>'Email ID')); ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php echo form_input($phone + array('class' => 'form-control', 'placeholder'=>'Phone Number')); ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php echo form_input($username + array('class' => 'form-control', 'placeholder'=>'User Name')); ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php echo form_input($password + array('class' => 'form-control', 'placeholder'=>'Password')); ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php echo form_input($password_confirm + array('class' => 'form-control', 'placeholder'=>'Confirm Password')); ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-12">
-                            <?php
-                            $publickey = "6LctLfISAAAAAEWmA7GBCAJC7SL4bzFc5jZuDA0O";
-                            echo recaptcha_get_html($publickey);
-                            ?>
-                        </div> 
-                    </div>
-                    <div class="form-group">
-                        <div class ="col-md-4">
-                            <?php echo form_input($submit_create_manager + array('class' => 'form-control btn-success')); ?>
-                        </div> 
-                    </div>
-                    
-                <?php echo form_close(); ?>
-            </div>
+            <div class="col-md-1"></div>
         </div>
     </div>
 </div>
+
+<div class="container-fluid">
+    <div class="row padding_before_image">
+        <div class="col-md-offset-1 col-md-5">
+            <div class="left_text_style col-xs-12 col-sm-12">A complete business account solution</div>
+        </div>
+        <div class="col-md-offset-1 col-md-4">
+            <div class="right_text_style col-xs-12 col-sm-12">Sign Up</div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-7">
+            <div class="col-xs-12 col-sm-12">
+                <img class="img-responsive" src="<?php echo base_url(); ?>resources/images/login_left_image.png">
+                <div class="form-group"></div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="col-xs-12 col-sm-12">
+                <?php echo form_open(LOGIN_URI, array('id' => 'form_create_admin', 'class' => 'form-horizontal')); ?>
+                <div class="row">
+                    <div class="col-md-12 col-xs-12 col-sm-12 message_style">
+                        <?php echo $message2; ?>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-12 col-xs-12 col-sm-12 ">
+                        <?php echo form_input($first_name + array('class' => 'form-control', 'placeholder' => 'First Name')); ?>
+                    </div> 
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-12 col-xs-12 col-sm-12">
+                        <?php echo form_input($last_name + array('class' => 'form-control', 'placeholder' => 'Last Name')); ?>
+                    </div> 
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-12 col-xs-12 col-sm-12">
+                        <?php echo form_input($email + array('class' => 'form-control', 'placeholder' => 'Email ID')); ?>
+                    </div> 
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-12 col-xs-12 col-sm-12">
+                        <?php echo form_input($phone + array('class' => 'form-control', 'placeholder' => 'Phone Number')); ?>
+                    </div> 
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-12 col-xs-12 col-sm-12">
+                        <?php echo form_input($username + array('class' => 'form-control', 'placeholder' => 'User Name')); ?>
+                    </div> 
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-12 col-xs-12 col-sm-12">
+                        <?php echo form_input($password + array('class' => 'form-control', 'placeholder' => 'Password')); ?>
+                    </div> 
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-12 col-xs-12 col-sm-12">
+                        <?php echo form_input($password_confirm + array('class' => 'form-control', 'placeholder' => 'Confirm Password')); ?>
+                    </div> 
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-11 col-xs-12 col-sm-12">
+                        <?php
+                        $publickey = "6LctLfISAAAAAEWmA7GBCAJC7SL4bzFc5jZuDA0O";
+                        echo recaptcha_get_html($publickey);
+                        ?>
+                    </div> 
+                    <div class ="col-md-1"></div>
+                </div>
+                <div class="row form-group">
+                    <div class ="col-md-4 col-xs-5 col-sm-5">
+                        <?php echo form_input($submit_create_manager + array('class' => 'form-control btn-success')); ?>
+                    </div> 
+                    <div class="col-md-8"></div>
+                </div>
+                <?php echo form_close(); ?>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="footer_style"></div>
+        </div>
+    </div>
+</div>
+
+
